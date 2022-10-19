1c0bb0cfaea0aab2c6522eecd2bef58aa2e05b06
Facu Benitez <facubenitez2001@hotmail.com.ar>
2022-10-18 21:17:13 -0300

    Cambios cp_insert
    Cambios pequeños, falta implementar un metodo busqueda de posicion valida

--- colacp.c
+++ colacp.c
@@ -3,10 +3,61 @@
 
 #include "colacp.h"
 
+const int TRUE = 1;
+const int FALSE = 0;
+const int CCP_NO_INI = -1;
+const void* POS_NULA = NULL;
+const void* ELE_NULO = NULL;
+
+//TODO: implementar busqueda de la primer posicion valida para insertar un nuevo nodo
+TNodo encontrarPrimerPosNula(TNodo nodo){
+    return nodo;
+}
+
 TColaCP crear_cola_cp(int (*f)(TEntrada, TEntrada)){
     TColaCP nuevaCola = malloc(sizeof(struct cola_con_prioridad));
     nuevaCola->cantidad_elementos = 0;
+    nuevaCola->comparador = *f;
+    nuevaCola->raiz = POS_NULA;
+    return nuevaCola;
 }
+
+int cp_insertar(TColaCP cola, TEntrada entr){
+
+    TNodo nuevoNodo = malloc(sizeof(struct nodo));
+    nuevoNodo->entrada = entr;
+
+    nuevoNodo->hijo_izquierdo = ELE_NULO;
+    nuevoNodo->hijo_derecho = ELE_NULO;
+
+    //Inserto raiz, si no existe una
+    if(cola->raiz==ELE_NULO)
+        cola->raiz = nuevoNodo;
+
+    //Si hay raiz, busco un lugar para meter la nueva entrada
+    else {
+
+        TNodo padre = encontrarPrimerPosNula(cola->raiz);
+        nuevoNodo->padre = padre;
+
+        if(padre->hijo_izquierdo==ELE_NULO)
+            padre->hijo_izquierdo = nuevoNodo;
+
+        else if(padre->hijo_derecho==ELE_NULO)
+            padre->hijo_derecho = nuevoNodo;
+
+        else{
+            free(nuevoNodo);
+            return 0;
+        }
+
+    }
+    cola->cantidad_elementos++;
+    return 1;
+}
+
+
+
 
 
 
--- main.c
+++ main.c
@@ -3,8 +3,61 @@
 
 #include "colacp.h";
 
+int compara(TEntrada e1, TEntrada e2){
+    TClave clave1 = e1->clave;
+    TClave clave2 = e2->clave;
+
+    if(clave1 > clave2)
+        return 1;
+    else if(clave1 == clave2)
+        return 0;
+    else
+        return -1;
+}
+
+void recorridoInorden(TNodo nodo){
+    if(nodo!=ELE_NULO){
+
+        if(nodo->hijo_izquierdo!=ELE_NULO)
+            recorridoInorden(nodo->hijo_izquierdo);
+
+        printf("%d %d \n",nodo->entrada->clave, nodo->entrada->valor);
+
+        if(nodo->hijo_derecho!=ELE_NULO)
+            recorridoInorden(nodo->hijo_derecho);
+
+    }
+}
+
 int main()
 {
-    printf("Size of void pointer: %i\n", sizeof(TClave));
+    TColaCP cola = crear_cola_cp(compara);
+    TEntrada e = malloc(sizeof(TEntrada));
+    TEntrada e2 = malloc(sizeof(TEntrada));
+    TEntrada e3 = malloc(sizeof(TEntrada));
+    TEntrada e4 = malloc(sizeof(TEntrada));
+
+    e->clave = 111;
+    e->valor = 164;
+
+    e2->clave = 222;
+    e2->valor = 238;
+
+    e3->clave = 333;
+    e3->valor = 379;
+
+    e4->clave = 444;
+    e4->valor = 412;
+
+    cp_insertar(cola, e);
+    cp_insertar(cola, e2);
+    cp_insertar(cola, e3);
+    cp_insertar(cola, e4);
+
+    //printf("%i \n",cp_insertar(cola, e2));
+    printf("Recorrido inorder: \n");
+    recorridoInorden(cola->raiz);
+
+    printf("Cantidad de elementos: %i \n", cola->cantidad_elementos);
     return 0;
 }
