81b2c85405580821a1c18db534b7805889206edb
Facundo Baruffetti <facubaru4@gmail.com>
2022-11-02 09:49:35 -0300

    Correcciones varias post consulta 1/11

--- planificador.c
+++ planificador.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "colacp.h"
 
@@ -49,38 +50,92 @@ int comparadorMax(TEntrada e1, TEntrada e2){
 
 void noEliminarEntrada(TEntrada e){
 }
-/*  argc = define la cantidad de parámetros por consola, más el nombre del programa
-    args = es el arreglo donde se encuentran todos los parámetros por consola         */
-int main(int argc, char ** args)
-{
-    //printf("Contenido texto: %s", args[1][0]);
 
-    TColaCP colaMin = crear_cola_cp(comparadorMin);
-    TColaCP colaMax = crear_cola_cp(comparadorMax);
-    TColaCP colaReducirHoras = crear_cola_cp(comparadorMin);
+int main(int argc, char * args[]){
 
-    //Empieza como el 1er valor que levantamos del archivo de texto, cambia en la 3er cola recien
     TCiudad ubicacionActual;
+    TEntrada entradas[100];
+    TEntrada entradasRH[100];
+
+    FILE * archivo;
+    archivo = fopen(args[1], "r");
+    if(archivo == NULL){
+        perror("Se ingreso un archivo nulo");
+        exit(-1);
+    }
+    char cadena[300];
+    fscanf(archivo, %s, cadena); ///CONSULTAR SI ES LO MEJOR O SI DEBERIAMOS USAR OTRA FUNCION
+    char delimitador[] = ";\n";
+    char * token = strtok(cadena, delimitador);
+    TCiudad aux;
+    ubicacionActual -> pos_x = -1;
+    ubicacionActual-> pos_y = -1;
+    aux -> pos_x = -1;
+    aux -> pos_y = -1;
+    int indice = 0;
+
+    if(token!= NULL){
+        while(token != NULL){
+            //1er caso, seteamos la ubicacion actual
+            if(ubicacionActual->pos_x == -1 || ubicacionActual->pos_y == -1){
+                if(ubicacionActual->pos_x == -1)
+                    ubicacionActual->pos_x = atoi(token);
+                else{
+                    if(ubicacionActual->pos_x != -1 && ubicacionActual->pos_y == -1)
+                        ubicacionActual->pos_x = atoi(token);
+                }
+            }
+            //2do caso, construimos una nueva entrada
+            else{
+                if(aux->nombre == NULL)
+                    strcpy(aux->nombre, token);
+                else{
+                    if(aux->pos_x == -1)
+                        aux->pos_x = atoi(token);
+                    else{
+                        if(aux->pos_y == -1){
+                            aux->pos_y = atoi(token);
+                            //Una vez que seteamos todos los valores de una ciudad, creamos la
+                            //entrada y la ubicamos en las colecciones de entradas.
+                            ///FALTA VER MANEJO DE MEMORIA
+                            TEntrada eAux;
+                            TEntrada eAuxRH;
+                            eAux->clave = (int) distancia(ubicacionActual, aux);
+                            eAuxRH->clave = (int) distancia(ubicacionActual, aux);
+                            eAux->valor = aux;
+                            eAuxRH->valor = aux;
+                            entradas[indice] = eAux;
+                            indice++;
+                            TCiudad aux2;
+                            aux2->pos_x = -1;
+                            aux2->pos_y = -1;
+                            aux = aux2;
+                        }
+                    }
+                }
+
+            }
+            token = strtok(NULL, delimitador);
+        }
+    }
+
     int horasDeManejo = 0;
-    /*
-        Levantamos el archivo de texto
-        asignamos ubicacionActual
-        para cada posicion del arreglo (a partir de la 2da)
-            creamos una entrada del tipo <Distancia al origen, ciudad>
-            creamos una entrada igual, pero que vamos a usar para ReducirHoras
-            hacer nodos enlazados
-            */
-
-            cp_insertar(colaMin, nuevaEntrada);
-            cp_insertar(colaMax, nuevaEntrada);
-            cp_insertar(colaReducirHoras, nuevaEntradaRH);
-
-        int op0 = 0;
-
+    int op0 = 0;
+    printf("Si desea finalizar la ejecucion, inserte 0 \n");
+    printf("Si desea mostrar ascendente, inserte 1 \n");
+    printf("Si desea mostrar descendente, inserte 2 \n");
+    printf("Si desea reducir horas de manejo, inserte 3 \n");
         while(op0 == 0){
-        //Preguntamos al usuario que quiere hacer
+        printf("Indique la opcion deseada: ");
+        int opcion = -1
+        scanf(); //Hay que hacerlo
+        printf("\n");
 
-            if(/*Ingresar 0 si quiere terminar la ejecucion*/){
+        TColaCP colaMin;
+        TColaCP colaMax;
+        TColaCP colaReducirHoras;
+
+            if(opcion == 0){
                 if(colaMin != NULL){
                     cp_destruir(colaMin, noEliminarEntrada);
                     colaMin = NULL;
@@ -97,24 +152,30 @@ int main(int argc, char ** args)
                 printf("Fin de la ejecucion \n");
             }
 
-            if(/*Ingresar 1 si quiere ver ascendente*/){
+            if(opcion == 1){
             if(colaMin == NULL)
                 printf("Esta operacion ya se ejecuto previamente \n");
             else{
+                colaMin = crear_cola_cp(comparadorMin);
+                for(int i = 0; i < indice; i++)
+                    cp_insertar(colaMin, entradas[i]);
                 printf("Mostrar ascendente: \n");
                 while(colaMin -> cantidad_elementos != 0){
                     TEntrada actual = cp_eliminar(colaMin);
-                    printf("%s \n", *(actual->valor->nombre));
+                    printf("%s \n", *actual->valor->nombre);
                 }
                 cp_destruir(colaMin, noEliminarEntrada);
                 colaMin = NULL;
             }
             }
 
-           if(/*Ingresar 2 si quiere ver descendente*/){
+           if(opcion == 2){
            if(colaMax == NULL)
                 printf("Esta operacion ya se ejecuto previamente \n");
             else{
+                colaMax = crear_cola_cp(comparadorMax);
+                for(int i = 0; i < indice; i++)
+                    cp_insertar(colaMax, entradas[i]);
                 printf("Mostrar descendente: \n");
                 while(colaMax ->cantidad_elementos != 0){
                     TEntrada actual = cp_eliminar(colaMax);
@@ -125,15 +186,18 @@ int main(int argc, char ** args)
             }
            }
 
-            if(/*Ingresar 3 si quiere reducir horas de manejo*/){
+            if(opcion == 3){
             if(colaReducirHoras == NULL)
                 printf("Esta operacion ya se ejecuto previamente \n");
             else{
+                colaReducirHoras = crear_cola_cp(comparadorMin);
+                for(int i = 0; i < indice; i++)
+                    cp_insertar(colaReducirHoras, entradasRH[i]);
                 printf("Mostrar descendente: \n");
                 while(colaReducirHoras ->cantidad_elementos != 0){
                     TEntrada actual = cp_eliminar(colaReducirHoras);
                     printf("%s \n", *(actual->valor->nombre));
-                    horasDeManejo = horasDeManejo + actual.clave;
+                    horasDeManejo = horasDeManejo + actual->clave;
                     ubicacionActual = actual;
                     /*  y para cada elemento de la cola, calculamos su distancia con la nueva ubicacion e insertamos en colalAUX (Hacer malloc correspondientes)
                         colaReducirHoras = colaAUX
